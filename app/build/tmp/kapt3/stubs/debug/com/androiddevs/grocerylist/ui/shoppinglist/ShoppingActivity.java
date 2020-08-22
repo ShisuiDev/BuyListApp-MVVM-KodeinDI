6c2dd2ac5363c5ba19f3966e97eab573c6e641bc
ShisuiDev <nugrahaadi733@gmail.com>
2020-08-22 16:13:27 +0700

    push first

---
+++ app/build/tmp/kapt3/stubs/debug/com/androiddevs/grocerylist/ui/shoppinglist/ShoppingActivity.java
@@ -0,0 +1,51 @@
+package com.androiddevs.grocerylist.ui.shoppinglist;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 15}, bv = {1, 0, 3}, k = 1, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u00012\u00020\u0002B\u0005\u00a2\u0006\u0002\u0010\u0003J\u0012\u0010\u0015\u001a\u00020\u00162\b\u0010\u0017\u001a\u0004\u0018\u00010\u0018H\u0014R\u001b\u0010\u0004\u001a\u00020\u00058BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\b\u0010\t\u001a\u0004\b\u0006\u0010\u0007R\u001b\u0010\n\u001a\u00020\u000b8VX\u0096\u0084\u0002\u00a2\u0006\f\n\u0004\b\u000e\u0010\t\u001a\u0004\b\f\u0010\rR\u001a\u0010\u000f\u001a\u00020\u0010X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0011\u0010\u0012\"\u0004\b\u0013\u0010\u0014\u00a8\u0006\u0019"}, d2 = {"Lcom/androiddevs/grocerylist/ui/shoppinglist/ShoppingActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "Lorg/kodein/di/KodeinAware;", "()V", "factory", "Lcom/androiddevs/grocerylist/ui/shoppinglist/ShoppingViewModelFactory;", "getFactory", "()Lcom/androiddevs/grocerylist/ui/shoppinglist/ShoppingViewModelFactory;", "factory$delegate", "Lkotlin/Lazy;", "kodein", "Lorg/kodein/di/Kodein;", "getKodein", "()Lorg/kodein/di/Kodein;", "kodein$delegate", "viewModel", "Lcom/androiddevs/grocerylist/ui/shoppinglist/ShoppingViewModel;", "getViewModel", "()Lcom/androiddevs/grocerylist/ui/shoppinglist/ShoppingViewModel;", "setViewModel", "(Lcom/androiddevs/grocerylist/ui/shoppinglist/ShoppingViewModel;)V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class ShoppingActivity extends androidx.appcompat.app.AppCompatActivity implements org.kodein.di.KodeinAware {
+    @org.jetbrains.annotations.NotNull()
+    private final kotlin.Lazy kodein$delegate = null;
+    private final kotlin.Lazy factory$delegate = null;
+    @org.jetbrains.annotations.NotNull()
+    public com.androiddevs.grocerylist.ui.shoppinglist.ShoppingViewModel viewModel;
+    private java.util.HashMap _$_findViewCache;
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public org.kodein.di.Kodein getKodein() {
+        return null;
+    }
+    
+    private final com.androiddevs.grocerylist.ui.shoppinglist.ShoppingViewModelFactory getFactory() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.androiddevs.grocerylist.ui.shoppinglist.ShoppingViewModel getViewModel() {
+        return null;
+    }
+    
+    public final void setViewModel(@org.jetbrains.annotations.NotNull()
+    com.androiddevs.grocerylist.ui.shoppinglist.ShoppingViewModel p0) {
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    public ShoppingActivity() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public org.kodein.di.KodeinContext<?> getKodeinContext() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public org.kodein.di.KodeinTrigger getKodeinTrigger() {
+        return null;
+    }
+}
